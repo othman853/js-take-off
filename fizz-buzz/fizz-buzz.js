86d047f87d0560f3f82af5c43d5a20cf2d6fe19d
othman853 <anuar.yasser@gmail.com>
2017-09-29 00:39:22 -0300

    Initial implementation

---
+++ fizz-buzz/fizz-buzz.js
@@ -0,0 +1,13 @@
+module.exports = (numero) => {
+  if (numero === 3) {
+    return 'fizz'
+  }
+
+  if (numero === 5) {
+    return 'buzz'
+  }
+
+  if (numero === 15) {
+    return 'fizzbuzz'
+  }
+}
